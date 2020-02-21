f824dea9815887370bb7a2ade526249315f90e72
MitreaDoru <mitrea.doru.catalin@gmail.com>
2020-02-21 15:47:34 +0200

    ch81.py

---
+++ ch81.py
@@ -0,0 +1,19 @@
+import pyinputplus as pyip
+import random, time
+numberOfQuestions = 10
+correctAnswers = 0
+for numberQuestion in range(numberOfQuestions):
+    num1 = random.randint(0,9)
+    num2 = random.randint(0,9)
+    print(numberQuestion+1, num1, num2)
+    try:
+        pyip.inputStr(allowRegexes=['^%s$' % (num1 * num2)], blockRegexes=[('.*','Incorrect!')],timeout=8, limit=3)
+    except pyip.TimeoutException:
+        print('Out of time!')
+    except pyip.RetryLimitException:
+        print('Out of tries!')
+    else:
+        print('Correct!')
+        correctAnswers += 1
+time.sleep(1) 
+print('Score: %s / %s' % (correctAnswers, numberOfQuestions))
